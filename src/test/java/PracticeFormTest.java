9c22d8c8c34df755d01ec0fdd1d23f7565c8df09
Sergei Volkov <Sneedjik@gmail.com>
2024-09-14 02:21:03 +0600

    changed the method of sex selection and hobbies

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
@@ -19,7 +20,7 @@ public class PracticeFormTest {
         $("#firstName").setValue("Bony");
         $("#lastName").setValue("Skye");
         $("#userEmail").setValue("Bony@test.com");
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("9659112131");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").$("option[value='1991']").click();
@@ -27,7 +28,7 @@ public class PracticeFormTest {
         $(".react-datepicker__day--007:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("One-Punch Man.jpg");
         $("#currentAddress").setValue("Street 1");
         $("#react-select-3-input").setValue("NCR").pressEnter();
@@ -43,7 +44,7 @@ public class PracticeFormTest {
              text("9659112131"),            // Проверка телефона
              text("07 October,1991"),       // Проверка даты рождения
              text("Maths"),                 // Проверка предметов
-             text("Sports"),                // Проверка хобби
+             text("Reading"),                // Проверка хобби
              text("One-Punch Man.jpg"),     // Проверка загруженной картинки
              text("Street 1"),              // Проверка адреса
              text("NCR Delhi")              // Проверка штата и города
